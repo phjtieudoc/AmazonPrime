4c4aa2623a429b872b8edd881cfae946087f4ff6
Drake Strike <phjtieudoc@users.noreply.github.com>
2020-10-19 18:30:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,80 +21,30 @@ $(function()
 	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
 	
 	var songs = [{
-		artist: "Mr.Siro",
-		name: "Lắng Nghe Nước Mắt",
-		url: "https://docs.google.com/uc?export=download&id=1Ecg3BZ1CItVsfusAnykh1MfaML4SNhT-",
-		picture: "https://avatar-nct.nixcdn.com/song/2018/05/06/5/1/b/4/1525584753612_640.jpg"
+		artist: "羽肿",
+		name: "Windy Hill",
+		url: "https://docs.google.com/uc?export=download&id=1WpE5nzgEHFAhMGsKpTR0Z9KCuXJKXOFP",
+		picture: "https://i1.sndcdn.com/artworks-000384500082-pnvos5-t500x500.jpg"
 	}, {
-    artist: "Hà Anh Tuấn",
-    name: "Tháng Tư Là Lời Nói Dối Của Em",
-    url: "https://docs.google.com/uc?export=download&id=1D8e6YTLNHDEooxq_VbxP3vScyOcm8QHm",
-    picture: "https://avatar-nct.nixcdn.com/song/2017/09/22/1/a/3/6/1506063956390_640.jpg"
+    artist: "BigRicePiano",
+    name: "A Wish To The Sky",
+    url: "https://docs.google.com/uc?export=download&id=1M2QxUToOBUKfdyV2wtqJfQdz1GL7DKXm",
+    picture: "https://f4.bcbits.com/img/a2288896432_16.jpg"
 }, {
-    artist: "Tino",
-    name: "Ừ Có Anh Đây",
-    url: "https://docs.google.com/uc?export=download&id=1emYz8dFA0a0AwtVZGiMqk9_LV9USvEC8",
-    picture: "https://photo-zmp3.zadn.vn/covers/0/4/047305f465f0aa64c22ed49787b02548_1510199251.jpg"
+    artist: "BigRicePiano",
+    name: "The Heart's Tale",
+    url: "https://docs.google.com/uc?export=download&id=1LpOOc9HxXAZ6mNX4guebPM-aZtzuDhAs",
+    picture: "https://f4.bcbits.com/img/a3663078782_16.jpg"
 }, {
-    artist: "JayKii",
-    name: "Sao Em Nỡ",
-    url: "https://docs.google.com/uc?export=download&id=1Yc_IIps7kjGtv3TYDhc9uTne-blEPF3Y",
-    picture: "https://data.chiasenhac.com/data/cover/90/89895.jpg"
+    artist: "BGM Maker",
+    name: "【泣けるサントラ】切ないけど、どこか暖かい、心にしみるピアノ曲【作業用・睡眠用BGM】癒しの音楽",
+    url: "https://docs.google.com/uc?export=download&id=1Bmf__XeftXWu-zDzwr3pF0oKMt3sVP5D",
+    picture: "https://i.ytimg.com/vi/0GuC7TxvI1M/maxresdefault.jpg"
 }, {
-    artist: "Nguyễn Đình Vũ",
-    name: "Chúng Ta Dừng Lại Ở Đây Thôi",
-    url: "https://docs.google.com/uc?export=download&id=1sCCIGG-lWUkibZg8SeUYBkp6mvGfF655",
-    picture: "https://avatar-nct.nixcdn.com/song/2019/09/26/a/a/9/f/1569463314685_640.jpg"
-}, {
-    artist: "Thanh Hưng",
-    name: "Sai Người Sai Thời Điểm",
-    url: "https://docs.google.com/uc?export=download&id=1Y3AdWH6PyKZ5ODwjr1XbaQgGvXQP0PEw",
-    picture: "https://data.chiasenhac.com/data/cover/89/88857.jpg"
-}, {
-    artist: "Soobin Hoàng Sơn",
-    name: "Yêu Thương Ngày Đó",
-    url: "https://docs.google.com/uc?export=download&id=1INJlKojJxuZB0Q7aRkMV42Pcy_NsRioj",
-    picture: "https://photo-zmp3.zadn.vn/cover/d/0/b/8/d0b8df07498b8f0549eed787569efba6.jpg"
-}, {
-    artist: "Trung Quân",
-    name: "Cánh Đồng Yêu Thương",
-    url: "https://docs.google.com/uc?export=download&id=1_qYzpWclu5XLMAE4PynQZ320wbFVvyJa",
-    picture: "https://data.chiasenhac.com/data/cover/71/70389.jpg"
-}, {
-    artist: "Hồ Phong An",
-    name: "Giá Như Anh Là Người Vô Tâm",
-    url: "https://docs.google.com/uc?export=download&id=19OQl77m_eOLJejlqrRY0-eZa0qARJtUD",
-    picture: "https://data.chiasenhac.com/data/cover/94/93072.jpg"
-}, {
-    artist: "Vương Anh Tú",
-    name: "Anh Biết Anh Sai Rồi",
-    url: "https://docs.google.com/uc?export=download&id=1dHf8OzTl1PHwCVk6CJwxPWus_RV-14YR",
-    picture: "https://data.chiasenhac.com/data/cover/72/71073.jpg"
-}, {
-    artist: "Mr.Siro",
-    name: "Đừng Rời Xa Anh",
-    url: "https://docs.google.com/uc?export=download&id=1EVMomTHHEsDb6mRH3NUzROg2JAyWPyUl",
-    picture: "https://data.chiasenhac.com/data/cover/68/67050.jpg"
-}, {
-    artist: "Vương Anh Tú",
-    name: "Anh Sẽ Ổn Thôi",
-    url: "https://docs.google.com/uc?export=download&id=1yNBdRY7Yn6-_n1lQEN3DIhE1yUwbVHi4",
-    picture: "https://photo-zmp3.zadn.vn/covers/7/8/78822d9ca7bae4240a7f9048e776dc4e_1474520373.jpg"
-},{
-    artist: "Lương Gia Hùng",
-    name: "Cô Đơn Sẽ Tốt Hơn",
-    url: "https://docs.google.com/uc?export=download&id=18B074pWzzgVhbpCnjPYdx37k53-YeAhl",
-    picture: "https://zmp3-photo-fbcrawler.zadn.vn/cover/3/2/d/5/32d5cb46f04e2fccd0897770ae29f791.jpg"
-}, {
-    artist: "Soobin Hoàng Sơn",
-    name: "Xin Đừng Lặng Im",
-    url: "https://docs.google.com/uc?export=download&id=1e9TeZkwRCSkmk0trL2NnqKfPWS912dgf",
-    picture: "https://avatar-nct.nixcdn.com/song/2017/07/26/0/5/a/f/1501064298342_640.jpg"
-}, {
-    artist: "Dình Dũng",
-    name: "Sai Lầm Của Anh",
-    url: "https://docs.google.com/uc?export=download&id=1rxfB1BUGYbInBzBoqeBMwvILYBJOS-R-",
-    picture: "https://avatar-nct.nixcdn.com/song/2019/10/21/4/6/1/e/1571623691373_640.jpg"
+    artist: "BGM Maker",
+    name: "あの夏を思い出す、ちょっと切ない音楽【ピアノBGM】作ってみた #1",
+    url: "https://docs.google.com/uc?export=download&id=1eBVzc3BRi9DuvCKYeGMWfhCOK1rJKn3P",
+    picture: "https://i.ytimg.com/vi/PJFvJj1-wSY/maxresdefault.jpg"
 }];
 	
 	function shuffle(a) {
